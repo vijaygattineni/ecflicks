c1e8b216b48058c94416b956f9a1a1a89a1d8872
vgattineni <vgattineni@IM-LP-525.local>
2016-11-09 12:22:24 +0530

    DB Connection using public ip

--- db_connection/dbConnection.js
+++ db_connection/dbConnection.js
@@ -4,7 +4,7 @@
 
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/ecine'); // connecting to Local database
-mongoose.connect('mongodb://54.210.16.72:27017/ecine'); // connecting to Server database
+mongoose.connect('mongodb://localhost/ecine'); // connecting to Server database
 mongoose.connection.on('error', function (err) {
     console.log("Connection error");
 });
